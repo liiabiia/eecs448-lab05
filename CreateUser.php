29040c5e4fae2e87d244355d3cd9d1b10ecb4f32
Liia Butler <l173b625@ku.edu>
2016-03-15 14:34:39 -0500

    initial commit

---
+++ CreateUser.php
@@ -0,0 +1,8 @@
+<?php
+
+    $username = $_POST["Username"];
+    //check username if it's empty or if it already exists
+    if(!isset($username)){
+        //yell at user for not selling username
+    }
+?>
