2fe30bbe084dbaa2d2f0192e82a7ff0d4a8007f8
Liia Butler <l173b625@ku.edu>
2016-03-21 10:43:09 -0500

    Fix logic to detect if username does not exist.

--- CreatePosts.php
+++ CreatePosts.php
@@ -5,6 +5,7 @@
     }
     $username = $_POST["Username"];
     $content = $_POST["Post"];
+    $validName = "";
     //blank post and username
     if($username == ""){
         echo "Error: You must provide your username. I'm not saving this.";
@@ -15,16 +16,23 @@
     } else{
         $query = "SELECT user_id FROM Users WHERE user_id = '$username'";
         //check to make sure username exists
-        if(!($result = $mysqli->query($query))){
-            echo "Error: Username does not exist. I'm not saving this.";
-        } else{
+        if(($result = $mysqli->query($query))){
+            //echo "Error: Username does not exist. I'm not saving this.";
+            while($row = $result->fetch_assoc()){
+                $validName= $row['user_id'];
+            }
+        } 
+        if ($validName == $username) {
             $insert = "INSERT INTO Posts (content, author_id) VALUES ('$content','$username')";
             if($tryThis = $mysqli->query($insert)){
                 echo "Welcome back ". $username ."!!! Your post was successfully saved.";
             } else{
                 echo "Error: Something went wrong. Post not saved.";
             }
+        } else {
+            echo "Error: Username does not exist. I'm not saving this.";
         }
+        
     }
 
 //welcome back username, your post was succesfully saved.
